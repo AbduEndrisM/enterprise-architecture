5177754a87b3ac9b97d78132d17bbbbb45c7efbd
AbduEndrisM <beazsasm@gmail.com>
2019-05-28 04:28:32 -0500

    Homework 2 - Lesson 1 - starbuck

---
+++ WAA/Homework/LessonTwo/README.md
@@ -0,0 +1,1 @@
+## waa Web Project
---
+++ WAA/Homework/LessonTwo/src/main/java/com/waa/abdu/com/waa/abdu/iservice/DataFacade.java
@@ -0,0 +1,9 @@
+package com.waa.abdu.com.waa.abdu.iservice;
+
+import java.util.List;
+
+public interface DataFacade {
+
+    public String findPassword(String name );
+    public List<String> getAdvice(String roast);
+}
