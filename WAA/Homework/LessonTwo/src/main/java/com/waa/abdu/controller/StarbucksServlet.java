c4d03a8236e93643dfaa099d39339c1aefb77cd9
AbduEndrisM <beazsasm@gmail.com>
2019-05-28 04:37:14 -0500

    Homework 2 - Lesson 1 - starbuck

---
+++ WAA/Homework/LessonTwo/src/main/java/com/waa/abdu/controller/StarbucksServlet.java
@@ -0,0 +1,13 @@
+package com.waa.abdu.controller;
+
+import java.io.IOException;
+
+public class StarbucksServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
