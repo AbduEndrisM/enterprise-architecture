b87cd6b1eb55d6c73e419af463d4bde2d0e69c9f
abduendrism <beazsasm@gmail.com>
2019-06-27 23:20:27 -0500

    homework-two-hibernate

---
+++ EA/Homework/lesson-two/src/main/java/com/ea/abdu/domain/Car.java
@@ -0,0 +1,57 @@
+package com.ea.abdu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String brand;
+	private String year;
+	private double price;
+
+	public Car() {
+	}
+
+	public Car(String brand, String year, double price) {
+		this.brand = brand;
+		this.year = year;
+		this.price = price;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+}
